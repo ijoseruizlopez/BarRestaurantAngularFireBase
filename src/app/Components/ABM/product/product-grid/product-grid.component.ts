86b3bb923ac1772d43c6f57dcabe17abd61a1a82
ijoseruizlopez <ijoseruizlopez@gmail.com>
2018-10-17 16:14:04 -0300

    Acomodamos estilos de tablas y cambios generales en css y html de otros componentes

--- src/app/Components/ABM/product/product-grid/product-grid.component.ts
+++ src/app/Components/ABM/product/product-grid/product-grid.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirestoreProductService } from 'src/app/Services/firestore/productService/product-service.service';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material';
+
 
 @Component({
   selector: 'app-product-grid',
@@ -10,13 +13,15 @@ import { FirestoreProductService } from 'src/app/Services/firestore/productServi
 export class ProductGridComponent implements OnInit {
 
   displayedColumns: string[] = ['Icon', 'Carta','Clasificacion','Descripcion', 'Accion'];
-  public dataSource =[];
-
+  public dataSource =new MatTableDataSource([]);
+  @ViewChild(MatSort) sort: MatSort;
   
   constructor(private router: Router, private firestoreService: FirestoreProductService) { }
 
   ngOnInit() {
     this.getServices();
+    this.dataSource.sort=this.sort;
+  
   }
 
 
@@ -28,16 +33,19 @@ export class ProductGridComponent implements OnInit {
 
     this.firestoreService.GetProducts().subscribe((productsSnapshot) => {
      
-      this.dataSource = [];
+      var dataSourceAux = [];
       productsSnapshot.forEach((productData: any) => {
         var data = productData.payload.doc.data();
-        this.dataSource.push({
+        dataSourceAux.push({
           Id: productData.payload.doc.id,
           Carta: data.Carta==null ?  "": data.Carta,
           Clasificacion: data.Clasificacion==null ? "": data.Clasificacion,
           Descripcion: data.Descripcion
         });
+
       })
+      this.dataSource = new MatTableDataSource(dataSourceAux);
+      
     });
   }
 }
